b9bd6fa1daaa87b8ca20cbade8ab8c07e3603e32
mjbohn <github@mich3l.de>
2025-06-29 08:54:39 +0200

    itroducing IMemoryWatchForm

---
+++ CPU_emu/Interfaces/IMemoryWatchForm.cs
@@ -0,0 +1,14 @@
+﻿using CPU_emulator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPU_emu
+{
+    internal interface IMemoryWatchForm
+    {
+        
+    }
+}
