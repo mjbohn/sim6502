caf57f4e730d176e4d149ac273c3d05b71a5331d
Michael Bohn <mjbohn@gmail.com>
2025-07-18 14:34:44 +0200

    introduced IMemoryBus

---
+++ CPU_emu/Interfaces/IMemoryBus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPU_emu;
+
+internal interface IMemoryBus
+{
+    byte Read(ushort address);
+    void Write(ushort address, byte value);
+    void Load(ushort address, params byte[] data);
+    byte[] Dump(ushort address, int length);
+
+
+}
